0ac708195571aa50a8aff0e1a8472cbb3efef980
Leslie-vibin <leslievibin93@gmail.com>
2024-10-29 15:28:55 +0530

    3va

--- src/test/java/demo/TestScript1.java
+++ src/test/java/demo/TestScript1.java
@@ -1,5 +1,9 @@
 package demo;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
 
 public class TestScript1 {
@@ -7,7 +11,24 @@ public class TestScript1 {
     @Test
     public void  test() {
 		// TODO Auto-generated method stub
-		
+    	String browser=System.getProperty("browser");
+		if(browser.equalsIgnoreCase("chrome"))
+		{
+		WebDriver driver =new ChromeDriver();
+			
+		}
+		if(browser.equalsIgnoreCase("edge"))
+		{
+			WebDriver driver=new EdgeDriver();
+			
+		}
+		if(browser.equalsIgnoreCase("firefox"))
+		{
+			WebDriver	driver=new FirefoxDriver();
+			
+		}
+		//String username=System.getProperty("username");
+	    // String pass=System.getProperty("password");
 		//welcome
 		System.out.println("Hello");
 		//Thank you
